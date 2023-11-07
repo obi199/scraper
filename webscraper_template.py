112f0ed2b32c4aa4bedb88ef104562dcd41d69c3
obi199 <60046194+obi199@users.noreply.github.com>
2023-11-07 17:15:36 +0100

    Update webscraper_template.py
    update

--- webscraper_template.py
+++ webscraper_template.py
@@ -2,34 +2,30 @@
 from selenium import webdriver
 from selenium.webdriver import Firefox #Chrome
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from db_schema import table, engine
 from sqlalchemy.orm import sessionmaker
 
-
-def check_exists_by_xpath(xpath, driver):
-    try:
-        driver.find_element_by_xpath(xpath)
-    except NoSuchElementException:
-        return False
-    return True
-
-Session = sessionmaker(bind = engine)
-session = Session()
-
-ooptions = Options()
-options.headless = True
-options.add_argument("--window-size=1920,1200")
+options = Options()
+#options.add_argument('--headless')
+options.add_argument("--start-maximized")
 options.add_argument('log-level=3')
-driver = Firefox(options=options)
 
-base_url = ""
-url_list = []
-for url in url_list:
-    driver.get(url)
-    if check_exists_by_xpath(xpath, driver):
-        element1 = driver.find_elements_by_xpath('//html/body').text ###look for xpath elements
-    ins = table(name = element1) #insert in table
-    session.add(ins)
-    session.commit()
+driver = webdriver.Firefox(options=options) 
+
+url = "https://duckduckgo.com/"
+driver.get(url)
+element = "//*[@id="searchbox_input"]"
+driver.find_element(By.XPATH,element)
+
+###...do something here....
 
 driver.quit()
+
+###for database###
+#Session = sessionmaker(bind = engine)
+#session = Session()
+
+#ins = table(name = element) #insert in table
+#session.add(ins)
+#session.commit()
